921d9543008c05de1023be2a3ec2f1d42f3e423b
rebcaudle <rebcaudle@gmail.com>
2018-11-15 19:52:55 -0500

    Add files via upload

---
+++ ch20_ex1/model/category_db.php
@@ -0,0 +1,65 @@
+<?php
+function get_categories() {
+    global $db;
+    $query = 'SELECT * FROM categories
+              ORDER BY categoryID';
+    try {
+        $statement = $db->prepare($query);
+        $statement->execute();
+        $result = $statement->fetchAll();
+        $statement->closeCursor();
+        return $result;
+    } catch (PDOException $e) {
+        display_db_error($e->getMessage());
+    }
+}
+
+function get_category($category_id) {
+    global $db;
+    $query = 'SELECT * FROM categories
+              WHERE categoryID = :category_id';
+    try {
+        $statement = $db->prepare($query);
+        $statement->bindValue(':category_id', $category_id);
+        $statement->execute();
+        $result = $statement->fetch();
+        $statement->closeCursor();
+        return $result;
+    } catch (PDOException $e) {
+        display_db_error($e->getMessage());
+    }
+}
+
+function add_category($name) {
+    global $db;
+    $query = 'INSERT INTO categories (categoryName)
+              VALUES (:name)';
+    try {
+        $statement = $db->prepare($query);
+        $statement->bindValue(':name', $name);
+        $statement->execute();
+        $statement->closeCursor();
+
+        // Get the last product ID that was automatically generated
+        $category_id = $db->lastInsertId();
+        return $category_id;
+    } catch (PDOException $e) {
+        $error_message = $e->getMessage();
+        display_db_error($error_message);
+    }
+}
+
+function delete_category($category_id) {
+    global $db;
+    $query = 'DELETE FROM categories WHERE categoryID = :category_id';
+    try {
+        $statement = $db->prepare($query);
+        $statement->bindValue(':category_id', $category_id);
+        $statement->execute();
+        $statement->closeCursor();
+    } catch (PDOException $e) {
+        $error_message = $e->getMessage();
+        display_db_error($error_message);
+    }
+}
+?>
